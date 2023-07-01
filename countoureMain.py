cbf6bd9119d8344e0ea11175683f1a6000f9e270
kirpichnikov.a@insitech.ru <kirpichnikov.a@insitech.ru>
2023-07-01 14:14:35 +0300

    qq

--- countoureMain.py
+++ countoureMain.py
@@ -48,16 +48,6 @@ class svgPath:
                 path.C(item[1].x / dpi, item[1].y / dpi, item[2].x / dpi, item[2].y / dpi, item[3].x / dpi, item[3].y / dpi)
                 path.C(item[4].x / dpi, item[4].y / dpi, item[5].x / dpi, item[5].y / dpi, item[6].x / dpi, item[6].y / dpi)
         return
-    def createFlatCouture(self, lines):
-        for index in range(len(lines)):
-            line = lines[index]
-            pp0, pp1 = bezier.convertToPoint(line)
-            if index == 0:
-                self.addM(pp0)
-            else:
-                self.addL(pp0)
-            self.addL(pp1)
-        self.addZ()
     def testPointInCounture(self, lines, circles):
         circlesNew = []
         points=[]
@@ -82,4 +72,37 @@ class svgPath:
                     circle[2]=0
                 index = index+1
         
+        return
+    def centerLine(self, line):
+        pointS = Point(line[0][0], line[0][1])
+        pointE = Point(line[1][0], line[1][1])
+        xCenter = pointS.x + ((pointS.x - pointE.x)/2)
+        yCenter = pointS.y + ((pointS.y - pointE.y)/2)
+        return Point(xCenter, yCenter)
+        
+    def createFlatCouture(self, lines):
+        for index in range(len(lines)):
+            line = lines[index]
+            if index == 1:
+                lineP = lines[index-1]
+                lineN = lines[index+1]
+
+                pointS = Point(lineP[1][0], lineP[1][1])
+                ppStart = geometry.Point(pointS.x, pointS.y)
+
+                pointE = Point(lineN[0][0], lineN[0][1])
+                ppEnd = geometry.Point(pointE.x, pointE.y)
+                
+                ppDown = self.centerLine(line)
+                
+                self.UneckSvg(ppStart, 40, 60, ppDown, 60,40, ppEnd)
+                continue
+            pp0, pp1 = bezier.convertToPoint(line)
+            if index == 0:
+                self.addM(pp0)
+            else:
+                self.addL(pp0)
+            self.addL(pp1)
+        self.addZ()
+    def UneckSvg(self, ppStart, cornSize0, cornSize1, ppDown, cornSize2,cornSize3, ppEnd):
         return
